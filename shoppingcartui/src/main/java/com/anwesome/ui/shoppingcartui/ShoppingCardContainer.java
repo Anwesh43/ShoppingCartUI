4e46f3c9f3cb1b0d98aa87f5419d20a13f9016b8
Anwesh43 <anweshthecool0@gmail.com>
2017-06-01 14:29:30 +0530

    Adding ContainerLayout

--- shoppingcartui/src/main/java/com/anwesome/ui/shoppingcartui/ShoppingCardContainer.java
+++ shoppingcartui/src/main/java/com/anwesome/ui/shoppingcartui/ShoppingCardContainer.java
@@ -21,20 +21,40 @@ public class ShoppingCardContainer {
     private Activity activity;
     private boolean isShown = false;
     private ScrollView scrollView;
+    private int w,h;
     private ListLayout listLayout;
+    private ContainerLayout containerLayout;
+    private CartBar cartBar;
     public ShoppingCardContainer(Activity activity) {
         this.activity = activity;
         scrollView = new ScrollView(activity);
         listLayout = new ListLayout(activity);
+        containerLayout = new ContainerLayout(activity);
+        initDimension(activity);
+        cartBar = new CartBar(activity);
         scrollView.addView(listLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        containerLayout.addView(cartBar,new ViewGroup.LayoutParams(w,h/10));
+        containerLayout.addView(scrollView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
     }
     public void addItem(Bitmap bitmap,String title,String subTitle,float price) {
         listLayout.addItem(bitmap,title,subTitle,price);
     }
+    private void initDimension(Context context) {
+        DisplayManager displayManager = (DisplayManager)context.getSystemService(Context.DISPLAY_SERVICE);
+        Display display = displayManager.getDisplay(0);
+        if(display != null) {
+            Point size = new Point();
+            display.getRealSize(size);
+            w = size.x;
+            h = size.y;
+        }
+    }
     public void show() {
         if(!isShown) {
+
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-            activity.setContentView(scrollView);
+            activity.setContentView(containerLayout);
             if(activity instanceof AppCompatActivity) {
                 AppCompatActivity appCompatActivity = (AppCompatActivity)activity;
                 ActionBar actionBar = appCompatActivity.getSupportActionBar();
@@ -50,7 +70,6 @@ public class ShoppingCardContainer {
         }
     }
     private class ListLayout extends ViewGroup{
-        private int w,h;
         public void onMeasure(int wspec,int hspec) {
             int hMax = h/30;
             for(int i=0;i<getChildCount();i++) {
@@ -76,16 +95,32 @@ public class ShoppingCardContainer {
         }
         public ListLayout(Context context) {
             super(context);
-            initDimension(context);
+            //initDimension(context);
         }
-        private void initDimension(Context context) {
-            DisplayManager displayManager = (DisplayManager)context.getSystemService(Context.DISPLAY_SERVICE);
-            Display display = displayManager.getDisplay(0);
-            if(display != null) {
-                Point size = new Point();
-                display.getRealSize(size);
-                w = size.x;
-                h = size.y;
+    }
+    private class ContainerLayout extends ViewGroup {
+        public ContainerLayout(Context context) {
+            super(context);
+        }
+        public void onMeasure(int wspec,int hspec) {
+            int hmax = 0;
+            for(int i=0;i<getChildCount();i++) {
+                View child = getChildAt(i);
+                measureChild(child,wspec,hspec);
+                if(child instanceof CartBar || child instanceof ScrollView) {
+
+                }
+            }
+            setMeasuredDimension(w,h);
+        }
+        public void onLayout(boolean reloaded,int a,int b,int wa,int ha) {
+            int x = 0,y = 0;
+            for(int i=0;i<getChildCount();i++) {
+                View child = getChildAt(i);
+                if(child instanceof CartBar || child instanceof ScrollView) {
+                    child.layout(x,y,x+child.getMeasuredWidth(),y+child.getMeasuredHeight());
+                    y += (child.getMeasuredHeight());
+                }
             }
         }
     }
