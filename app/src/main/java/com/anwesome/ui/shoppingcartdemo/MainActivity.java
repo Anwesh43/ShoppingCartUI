9c74f40fcc25a7dbb25b03d2368e2f7a4dd7e6f4
Anwesh43 <anweshthecool0@gmail.com>
2017-06-01 07:48:27 +0530

    adding a shopping cardview

--- app/src/main/java/com/anwesome/ui/shoppingcartdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/shoppingcartdemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.anwesome.ui.shoppingcartdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.shoppingcartui.ShoppingCartCardView;
+import com.anwesome.ui.shoppingcartui.ShoppingCartItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
+        ShoppingCartItem shoppingCartItem = new ShoppingCartItem(bitmap,"Item 1","This item is dope",2000);
+        ShoppingCartCardView shoppingCartCardView = new ShoppingCartCardView(this,shoppingCartItem);
+        addContentView(shoppingCartCardView,new ViewGroup.LayoutParams(1080,1080));
+
     }
 }
